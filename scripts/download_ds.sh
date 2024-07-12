322ee1173eec3389db8be32e18c34f84b8733450
abuelnasr0 <abuelnasr70@gmail.com>
2024-07-13 00:22:48 +0300

    Update download script

--- scripts/download_ds.sh
+++ scripts/download_ds.sh
@@ -2,4 +2,4 @@
 source .env
 
 mkdir -p $WORKING_DIR/dataset
-curl -L "https://universe.roboflow.com/ds/oOA9yUpuh9?key=$ROBOFLOW_KEY" > $WORKING_DIR/dataset/roboflow.zip; unzip $WORKING_DIR/dataset/roboflow.zip -d $WORKING_DIR/dataset/roboflow.zip
+curl -L "https://universe.roboflow.com/ds/oOA9yUpuh9?key=$ROBOFLOW_KEY" > $WORKING_DIR/dataset/roboflow.zip; unzip $WORKING_DIR/dataset/roboflow.zip -d $WORKING_DIR/dataset;
