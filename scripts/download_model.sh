170df67a5ec8ef04fec9cbec5543c452b332374d
abuelnasr0 <abuelnasr70@gmail.com>
2024-07-19 21:27:55 +0300

    Better logging

--- scripts/download_model.sh
+++ scripts/download_model.sh
@@ -4,6 +4,11 @@ source .env
 mkdir -p $WORKING_DIR/models && cd $WORKING_DIR/models && mkdir -p pretrained
 for var in "$@"
 do
+    printf "\n"
+    echo "Downloading $var"  
+    printf "\n"
     curl -L  https://github.com/ultralytics/assets/releases/download/v8.2.0/$var > pretrained/$var; 
+    printf "\n"
     echo "$var downloaded"
+    printf "\n"
 done
