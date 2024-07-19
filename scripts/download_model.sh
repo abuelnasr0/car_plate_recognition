9baae513ffe4e8e5b428cbac4ec6cddd0657f489
abuelnasr0 <abuelnasr70@gmail.com>
2024-07-19 21:03:02 +0300

    Enable multi model download

--- scripts/download_model.sh
+++ scripts/download_model.sh
@@ -2,4 +2,8 @@
 source .env
 
 mkdir -p $WORKING_DIR/models && cd $WORKING_DIR/models && mkdir -p pretrained
-curl -L  https://github.com/ultralytics/assets/releases/download/v8.2.0/${1} > pretrained/${1}; 
+for var in "$@"
+do
+    curl -L  https://github.com/ultralytics/assets/releases/download/v8.2.0/$var > pretrained/$var; 
+    echo "$var downloaded"
+done
